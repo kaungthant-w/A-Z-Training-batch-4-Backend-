f6a3a5e4f1483d24e83f4df4651582304a5ee38b
“kaungthant-w” <kaungthant9513@gmail.com>
2022-12-17 23:44:05 +0630

    register page & remove background dark

--- Mini Login Project UI/header.php
+++ Mini Login Project UI/header.php
@@ -8,7 +8,7 @@
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body class="bg-dark">
+<body>
 
     <div class="container mt-4">
         <div class="row">
--- Mini Login Project UI/register.php
+++ Mini Login Project UI/register.php
@@ -3,7 +3,7 @@
 <div class="col-12 col-md-8">
     <div class="card">
         <div class="card-body">
-            <form action="">
+            <form action="" method="post">
                 <div class="mb-3">
                     <label for="name">Name</label>
                     <input type="text" name="name" id="name" placeholder="Enter Your name" class="form-control">
@@ -21,10 +21,37 @@
                     <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Enter Your confirm password" class="form-control">
                 </div>
                 <div class="mb-3">
-                    <input type="submit" value="Register" class="btn btn-dark float-end">
+                    <input type="submit" name="register" value="Register" class="btn btn-dark float-end">
                 </div>
             </form>
         </div>
     </div>
 </div>
 <?php include "footer.php"; ?>
+<?php
+
+if(isset($_POST['register'])) {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $confirmPassword = $_POST["confirmPassword"];
+    // echo strlen($password);
+    if($name !="" & $email !="" & $password !="" & $password != "" & $confirmPassword !="") {
+        if(strlen($password) >= 6 && strlen($confirmPassword) >= 6) {
+            if($password == $confirmPassword) {
+                $_SESSION["email"] = $email;
+                $_SESSION["password"] = password_hash($password, PASSWORD_BCRYPT);
+                echo "Regsiter Success!";
+            
+            } else {
+                echo "Password not same. Try Again!";
+            }
+        } else {
+            echo "Password must be grather than 6";
+        }
+    } else {
+        echo "Need to fill";
+    }
+}
+
+?>
