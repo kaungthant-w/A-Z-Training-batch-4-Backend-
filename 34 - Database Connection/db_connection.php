37946865355eaf364e2eb69d0d25340d62a79cb4
“kaungthant-w” <kaungthant9513@gmail.com>
2022-12-29 10:37:46 +0630

    34 - Database Connection

---
+++ 34 - Database Connection/db_connection.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>db connection</title>
+</head>
+<body>
+    <h1>Database connection running ...</h1>
+    <?php
+        // mysqli_connect('hostname', 'userName', 'password', 'databaseName');
+        $connection = mysqli_connect("localhost", 'root', '', 'test');
+        if($connection) {
+            echo "<h1>database connect success!</h1>";
+        } else {
+            die();
+        }
+    ?>
+</body>
+</html>
